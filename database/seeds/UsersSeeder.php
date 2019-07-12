3d251eec7ea93a8f034eec8fdf198138a8c3d5fb
fantazqis <33064566+fantazqis@users.noreply.github.com>
2019-07-12 21:06:21 +0700

    Add files via upload

---
+++ database/seeds/UsersSeeder.php
@@ -0,0 +1,100 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Role;
+use App\User;
+use App\Staff;
+use App\Member;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Membuat role admin
+        $adminRole = new Role();
+        $adminRole->name = "admin";
+        $adminRole->display_name = "Admin";
+        $adminRole->save();
+
+        // Membuat role kepala
+        $kepalaRole = new Role();
+        $kepalaRole->name = "kepala";
+        $kepalaRole->display_name = "Kepala Perpustakaan";
+        $kepalaRole->save();
+
+        // Membuat role member
+        $memberRole = new Role();
+        $memberRole->name = "member";
+        $memberRole->display_name = "Member";
+        $memberRole->save();
+
+        // Membuat role staff
+        $staffRole = new Role();
+        $staffRole->name = "staff";
+        $staffRole->display_name = "Staff";
+        $staffRole->save();
+
+        // Membuat sample admin
+        $admin = new User();
+        $admin->name = 'Administrator';
+        $admin->email = 'admin@perpustakaan.com';
+        $admin->password = bcrypt('rahasia');
+        $admin->role = 'admin';
+        $admin->is_verified = 1;
+        $admin->save();
+        $admin->attachRole($adminRole);
+
+        // Membuat sample kepala
+        $admin = new User();
+        $admin->name = 'Kepala Perpustakaan';
+        $admin->email = 'kepala@perpustakaan.com';
+        $admin->password = bcrypt('rahasia');
+        $admin->role = 'kepala';
+        $admin->is_verified = 1;
+        $admin->save();
+        $admin->attachRole($kepalaRole);
+
+        // Membuat sample member
+        $member = new User();
+        $member->name = "Sample Member";
+        $member->email = 'member@perpustakaan.com';
+        $member->password = bcrypt('rahasia');
+        $member->role = 'member';
+        $member->is_verified = 1;
+        $member->save();
+        $member->attachRole($memberRole);
+
+        $member_data = new Member();
+        $member_data->user_id = $member->id;
+        $member_data->kode_member = "KD-01";
+        $member_data->tempat_lahir = "Denpasar";
+        $member_data->tanggal_lahir = "1994-01-01";
+        $member_data->no_identitas = "123-123-123";
+        $member_data->alamat = "Sesetan";
+        $member_data->jenis_kelamin = "Laki-laki";
+        $member_data->save();
+
+        // Membuat sample member
+        $staff = new User();
+        $staff->name = "Sample Staff";
+        $staff->email = 'staff@perpustakaan.com';
+        $staff->password = bcrypt('rahasia');
+        $staff->role = 'staff';
+        $staff->is_verified = 1;
+        $staff->save();
+        $staff->attachRole($staffRole);
+
+        $staff_data = new Staff();
+        $staff_data->user_id = $staff->id;
+        $staff_data->nip = "123";
+        $staff_data->telp = "087858687999";
+        $staff_data->alamat = "Sesetan";
+        $staff_data->jenis_kelamin = "Laki-laki";
+        $staff_data->save();
+    }
+}
